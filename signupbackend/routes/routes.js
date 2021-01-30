fd05f1174020466851b3717b349a68a9fcf5e98b
Thomas Mullaney <tloring.mullaney@gmail.com>
2021-01-29 17:02:33 -0800

    creating signin feature. schema made, backend server made, and single route made. Connected to mongodb but not posting

---
+++ signupbackend/routes/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const signUpTemplateCopy = require('../models/signUpModel');
+
+router.post('/signup', (request, response) => {
+
+    const signedUpUser = new signUpTemplateCopy({
+        fullName:request.body.fullName,
+        userName:request.body.userName,
+        email:request.body.email,
+        password:request.body.password
+    })
+    signedUpUser.save()
+    .then(data => {
+        response.json(data)
+    })
+    .catch(error =>{
+        response.json(error)
+    })
+})
+
+
+module.exports = router
